91f826c5371a1dcb77c7271b8ccd3ff135790637
xiaonuo1326 <92018941+alazeprt@users.noreply.github.com>
2022-11-20 14:01:21 +0800

    Modify Textures

--- src/main/java/net/alazeprt/apcomputer/block/pc.java
+++ src/main/java/net/alazeprt/apcomputer/block/pc.java
@@ -1,11 +1,16 @@
 package net.alazeprt.apcomputer.block;
 
 import net.minecraft.block.*;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.DirectionProperty;
-
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class pc extends Block {
     public static final DirectionProperty FACING;
@@ -30,6 +35,9 @@ public class pc extends Block {
     public BlockState getPlacementState(ItemPlacementContext ctx) {
         return this.getDefaultState().with(FACING,ctx.getPlayerFacing().getOpposite());
     }
-
+    public ActionResult neighborUpdate(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        world.isReceivingRedstonePower(pos);
+        return ActionResult.SUCCESS;
+    }
 
 }
